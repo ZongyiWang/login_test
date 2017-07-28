4a291784905c40b5199e8522a03c7cf840c30788
Zongyi Wang <zongyiwang@MacBook-Pro-4.local>
2017-07-27 21:24:23 -0500

    new version

---
+++ front-end/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+import DashBoard from '@/components/DashBoard'
+
+Vue.use(Router)
+
+const routes = [
+	{
+	  path: '/',
+	  name: 'Home',
+	  component: Home
+	},
+	{
+	  path: '/signup',
+	  name: 'Signup',
+	  component: Signup
+	},
+	{
+	  path: '/login',
+	  name: 'Login',
+	  component: Login
+	},
+	{
+		path: '/dashboard',
+		name: 'DashBoard',
+		component: DashBoard
+	}
+]
+
+
+export default new Router({
+	mode: 'history',
+	routes: routes,
+	linkActiveClass: 'active',
+	scrollBehavior (to, from, savedPosition) {
+  		return { x: 0, y: 0 }
+	}
+})
